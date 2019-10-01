c426e6799bd5bd0a76ceb0583252226176633172
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-10-01 17:34:14 -0300

    refactor/style: melhorar a qualidade do código e estilo das páginas

--- src/app/pages/pagina404/pagina404.component.ts
+++ src/app/pages/pagina404/pagina404.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pagina404Component implements OnInit {
 
+  shortMsg = 'Oops! A página solicitada não foi encontrada.';
+  longMsg = 'A URL pode ter sido digitada incorretamente ou a página que você procura não está mais disponível.';
+
   constructor() { }
 
   ngOnInit() {
