44fd5fc599b605f222a8c2f78ab6d784c494fb50
mathnm <matheusnuma@gmail.com>
2019-10-06 15:01:11 -0300

    feat: implementar páginas solicitação e alteração de senha

--- src/app/modules/usuario/pages/usuario-page-alteracao-senha/usuario-page-alteracao-senha.component.ts
+++ src/app/modules/usuario/pages/usuario-page-alteracao-senha/usuario-page-alteracao-senha.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
+
+class ConfirmacaoSenhaErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return (!!(control && control.invalid && control.parent.dirty)
+      || !!(control && control.parent && control.parent.invalid && control.parent.dirty));
+  }
+}
 
 @Component({
   selector: 'app-usuario-page-alteracao-senha',
@@ -6,10 +15,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-page-alteracao-senha.component.css']
 })
 export class UsuarioPageAlteracaoSenhaComponent implements OnInit {
+  contaForm: FormGroup;
+  confirmacaoSenhaMatcher = new ConfirmacaoSenhaErrorStateMatcher();
+  esconderSenha = true;
 
   constructor() { }
 
   ngOnInit() {
+    this.contaForm = new FormGroup( {
+      senhas: new FormGroup({
+        senha: new FormControl('', Validators.required),
+        confirmacaoSenha: new FormControl('')
+      }, this.checarSenha),
+    });
   }
 
+  checarSenha(senhas: FormGroup) {
+    return senhas.get('senha').value === senhas.get('confirmacaoSenha').value ? null : {diferentes: true};
+  }
+
+  alteraSenha() {
+
+  }
 }
--- src/app/modules/usuario/pages/usuario-page-solicitacao-senha/usuario-page-solicitacao-senha.component.ts
+++ src/app/modules/usuario/pages/usuario-page-solicitacao-senha/usuario-page-solicitacao-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario-page-solicitacao-senha',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-page-solicitacao-senha.component.css']
 })
 export class UsuarioPageSolicitacaoSenhaComponent implements OnInit {
+  contaForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.contaForm = new FormGroup( {
+      email: new FormControl( '', [Validators.required, Validators.email])
+    });
   }
 
+  recupSenha() {
+
+  }
 }
