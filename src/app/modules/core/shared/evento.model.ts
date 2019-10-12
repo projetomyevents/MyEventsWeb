799f6b199df24417457c28e902289d588d559ef4
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-10-12 17:14:24 -0300

    feat: adicionar modelo de evento, adicionar docs ao modelo de usuário

---
+++ src/app/modules/core/shared/evento.model.ts
@@ -0,0 +1,25 @@
+/* Representa um evento. */
+export interface Evento {
+  /* A identificação do evento. */
+  id?: number;
+  /* O nome do evento. */
+  nome: string;
+  /* O data de inicio do evento. */
+  data: Date;
+  /* O preço de entrada do evento. */
+  preco?: string;
+  /* O limite de acompanhantes padrão de cada convidado. */
+  limiteAcompanhantes: number;
+  /* A idade mínima permitida no evento. */
+  idadeMinima?: number;
+  /* O traje recomendado para o evento. */
+  traje?: string;
+  /* A descrição do evento. */
+  descricao?: string;
+  /* A imagem ilustrativa do evento. */
+  imagem?: File;
+  /* O cronograma do evento. */
+  cronograma: string;
+  /* Os arquivos em anexo do evento. */
+  anexos?: FileList
+}
