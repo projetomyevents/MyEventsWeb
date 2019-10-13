0d52a5448b06e368f9a77567ba42f368b0313d38
VBViniciusVB <viniciusvianalaguna@hotmail.com>
2019-10-13 19:07:15 -0300

    feat(RF015): Adicionar página de cadastro de evento

--- src/app/modules/evento/evento.module.ts
+++ src/app/modules/evento/evento.module.ts
@@ -4,13 +4,19 @@ import { EventoPageCriacaoComponent } from './pages/evento-page-criacao/evento-p
 import { EventoRoutingModule } from './evento-routing.module';
 import { EventoPageListaComponent } from './pages/evento-page-lista/evento-page-lista.component';
 import { EventoPageVisaoGeralComponent } from './pages/evento-page-visao-geral/evento-page-visao-geral.component';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [EventoPageCriacaoComponent, EventoPageListaComponent, EventoPageVisaoGeralComponent],
   imports: [
     CommonModule,
-    EventoRoutingModule
+    EventoRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class EventoModule { }
