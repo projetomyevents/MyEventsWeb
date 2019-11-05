7bb89ee8df71daf42bdac7eaf2d2a7b057b8594b
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-05 17:37:30 -0200

    refactor: guardar os endpoints do usuário em uma variável local

--- src/app/modules/core/shared/user.service.ts
+++ src/app/modules/core/shared/user.service.ts
@@ -10,36 +10,37 @@ import { AppConfig } from '../../../config/app.config';
 export class UserService {
 
   url = AppConfig.serverUrl;
+  userEndpoint = EndpointsConfig.user;
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Promise<HttpResponse<any>> {
-    return this.http.post<any>(`${this.url}/${EndpointsConfig.user.login}`,
+    return this.http.post<any>(`${this.url}/${this.userEndpoint.login}`,
       {email, password}, {observe: 'response'}).toPromise();
   }
 
   register(newUser: NewUser): Promise<any> {
-    return this.http.post<any>(`${this.url}/${EndpointsConfig.user.register}`, newUser).toPromise();
+    return this.http.post<any>(`${this.url}/${this.userEndpoint.register}`, newUser).toPromise();
   }
 
   get(email: string): Promise<User> {
-    return this.http.get<User>(`${this.url}/${EndpointsConfig.user.getByEmail(email)}`).toPromise();
+    return this.http.get<User>(`${this.url}/${this.userEndpoint.getByEmail(email)}`).toPromise();
   }
 
   confirm(token: string): Promise<any> {
-    return this.http.get<any>(`${this.url}/${EndpointsConfig.user.getConfirm(token)}`).toPromise();
+    return this.http.get<any>(`${this.url}/${this.userEndpoint.getConfirm(token)}`).toPromise();
   }
 
   resendConfirmation(email: string): Promise<any> {
-    return this.http.get<any>(`${this.url}/${EndpointsConfig.user.getResendConfirmation(email)}`).toPromise();
+    return this.http.get<any>(`${this.url}/${this.userEndpoint.getResendConfirmation(email)}`).toPromise();
   }
 
   resetPassword(token: string, newPassword: NewPassword): Promise<any> {
-    return this.http.post<any>(`${this.url}/${EndpointsConfig.user.getPasswordReset(token)}`, newPassword).toPromise();
+    return this.http.post<any>(`${this.url}/${this.userEndpoint.getPasswordReset(token)}`, newPassword).toPromise();
   }
 
   sendPasswordReset(email: string): Promise<any> {
-    return this.http.get<any>(`${this.url}/${EndpointsConfig.user.getSendPasswordReset(email)}`).toPromise();
+    return this.http.get<any>(`${this.url}/${this.userEndpoint.getSendPasswordReset(email)}`).toPromise();
   }
 
 }
