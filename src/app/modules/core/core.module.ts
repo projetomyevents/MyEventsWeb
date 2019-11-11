4c36b8d165bedd6fef08109f27daf4279594923e
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-11 11:54:21 -0200

    feat: implementar spinner de loading

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule, MatInputModule, MatProgressBarModule, MatTooltipModule } from '@angular/material';
+import {
+  MatAutocompleteModule,
+  MatInputModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatTooltipModule
+} from '@angular/material';
 import { EmailInput } from './components/email-input/email-input.component';
 import { CPFInput } from './components/cpf-input/cpf-input.component';
 import { PhoneInput } from './components/phone-input/phone-input.component';
 import { PasswordStrengthBar } from './components/password-strength-bar/password-strength-bar.component';
 import { CEPInput } from './components/cep-input/cep-input.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 
 @NgModule({
-  declarations: [EmailInput, CPFInput, PhoneInput, PasswordStrengthBar, CEPInput],
+  declarations: [EmailInput, CPFInput, PhoneInput, PasswordStrengthBar, CEPInput, LoadingSpinnerComponent],
   exports: [
     EmailInput,
     CPFInput,
     PhoneInput,
     PasswordStrengthBar,
-    CEPInput
+    CEPInput,
+    LoadingSpinnerComponent
   ],
   imports: [
     CommonModule,
@@ -24,7 +32,8 @@ import { CEPInput } from './components/cep-input/cep-input.component';
     MatInputModule,
     MatAutocompleteModule,
     MatTooltipModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatProgressSpinnerModule
   ]
 })
 export class CoreModule {
