a638a7aba2b5d178310692012cea67e071ec4c54
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-10 22:36:25 -0200

    feat: implementar página de lista de eventos do usuário

--- src/app/modules/event/pages/event-page-list/event-page-list.component.ts
+++ src/app/modules/event/pages/event-page-list/event-page-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { EventService } from '../../shared/event.service';
+import { SimpleEvent } from '../../shared/event.model';
+import { ConfirmationDialogComponent } from '../../../../shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-event-page-list',
@@ -7,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventPageListComponent implements OnInit {
 
-  constructor() {
+  events: SimpleEvent[];
+
+  constructor(
+    private eventService: EventService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {
   }
 
   ngOnInit(): void {
+    this.updateEvents();
+  }
+
+  updateEvents(): void {
+    this.eventService.getAll().then(
+      (response: any) => {
+        this.events = response;
+      },
+      (err: any) => {
+        this.snackBar.open(err.message, 'OK', {panelClass: 'snack-bar-failure'});
+      });
+  }
+
+  async details(id: any): Promise<void> {
+    await this.router.navigateByUrl(`event/${id}`);
+  }
+
+  async cancel(event: SimpleEvent): Promise<void> {
+    this.dialog.open(ConfirmationDialogComponent, {
+      width: '400px',
+      data: {
+        title: 'Cancelar evento',
+        message: `Tem certeza que deseja CANCELAR o evento '${event.name}'?`,
+        accept: () => {
+          // TODO: this shit
+          new Audio('../../../../assets/i-giorno-giovanna-have-a-dream.mp3').play();
+        },
+        reject: () => null
+      }
+    });
   }
 
 }
