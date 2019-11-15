fdf196be78ec2945cd601a9afa67a0211c92d866
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-15 15:01:07 -0200

    feat: adicionar rotas com barra inicial

--- src/app/config/routes.config.ts
+++ src/app/config/routes.config.ts
@@ -22,4 +22,25 @@ const routesNames = {
 
 export const RoutesConfig: any = {
   routesNames,
+  routes: {
+    home: `/${routesNames.home}`,
+    error404: `/${routesNames.error404}`,
+    user: {
+      signin: `/${routesNames.user.signin}`,
+      signup: `/${routesNames.user.signup}`,
+      activate: `/${routesNames.user.activate}`,
+      resendActivation: `/${routesNames.user.resendActivation}`,
+      passwordReset: `/${routesNames.user.passwordReset}`,
+      sendPasswordReset: `/${routesNames.user.sendPasswordReset}`,
+    },
+    event: {
+      create: `/${routesNames.event.create}`,
+      event: `/${routesNames.event.event}`,
+      events: `/${routesNames.event.events}`,
+    },
+    guest: {
+      guests: `/${routesNames.guest.guests}`,
+      guestsEdit: `/${routesNames.guest.guestsEdit}`,
+    },
+  },
 };
